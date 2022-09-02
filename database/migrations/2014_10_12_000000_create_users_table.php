6a354ac01f6b45b5f97d84388eaee76c72f9bdfb
KankichiJibiki <kan.0408@outlook.jp>
2022-09-02 16:22:15 +0900

    Added condition for users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->uuid();
-            $table->string('username')->unique();
+            $table->string('username', '50')->unique();
             $table->string('first_name');
             $table->string('last_name');
             $table->string('email')->unique();
