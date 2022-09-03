202da944a55743582d1e9ba73a364a542e20a326
KankichiJibiki <kan.0408@outlook.jp>
2022-09-03 13:08:05 +0900

    Changed string limitation

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,10 +16,10 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->uuid();
-            $table->string('username', '50')->unique();
+            $table->string('username')->unique();
             $table->string('first_name');
             $table->string('last_name');
-            $table->string('email', '100')->unique();
+            $table->string('email')->unique();
             $table->string('avatar', '50')->nullable();
             // $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
